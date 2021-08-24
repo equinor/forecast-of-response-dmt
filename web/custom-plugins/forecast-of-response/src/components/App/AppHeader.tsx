ccf3e0575057688ad6290caa809c7f09c3007268
netr0m <moamu@equinor.com>
2021-08-24 15:33:33 +0200

    app header (topbar)

---
+++ web/custom-plugins/forecast-of-response/src/components/App/AppHeader.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { TopBar } from '@equinor/eds-core-react'
+import styled from 'styled-components'
+import Icon from '../Icons'
+import { TAppHeader } from '../../Types'
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  > * {
+    margin-left: 40px;
+  }
+`
+
+export default (props: TAppHeader): JSX.Element => {
+  const { appName } = props
+  return (
+    <TopBar>
+      <TopBar.Header>
+        <Icon name="waves" size={24} />
+        {appName}
+      </TopBar.Header>
+      <TopBar.Actions>
+        <Icons>
+          <Icon name="account_circle" size={24} title="user" />
+          <Icon name="notifications" size={24} title="notifications" />
+        </Icons>
+      </TopBar.Actions>
+    </TopBar>
+  )
+}
