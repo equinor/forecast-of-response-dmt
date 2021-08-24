e88abe50af292809943b2e0d3445567fda23771d
netr0m <moamu@equinor.com>
2021-08-24 15:32:00 +0200

    common file for icons used

---
+++ web/custom-plugins/forecast-of-response/src/components/Icons.ts
@@ -0,0 +1,26 @@
+import { Icon } from '@equinor/eds-core-react'
+import {
+  // AppHeader
+  waves,
+  notifications,
+  account_circle,
+  // AppMenu
+  home,
+  library,
+  list,
+  person,
+  add_circle_outlined,
+} from '@equinor/eds-icons'
+
+Icon.add({
+  waves,
+  notifications,
+  account_circle,
+  home,
+  library,
+  list,
+  person,
+  add_circle_outlined,
+})
+
+export default Icon
