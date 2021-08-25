ea3a37cea40bdc1191b38253d83aa7e1e1646cb0
netr0m <moamu@equinor.com>
2021-08-25 15:49:55 +0200

    undo deletion of for-operations/src

---
+++ web/custom-plugins/for-operations/src/Enums.ts
@@ -0,0 +1,11 @@
+export enum OperationStatus {
+  UPCOMING = 'Upcoming',
+  IN_PROGRESS = 'In progress',
+  CONCLUDED = 'Concluded',
+}
+
+export enum StepperState {
+  WAITING,
+  ACTIVE,
+  COMPLETED,
+}
