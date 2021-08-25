f662d55d14066873625ffddba5dbbd0ffb64eb7e
netr0m <moamu@equinor.com>
2021-08-25 15:30:51 +0200

    import type DmtSettings from dmt/core-plugins, remove obsolete TSettings type

--- web/custom-plugins/forecast-of-response/src/Types.ts
+++ web/custom-plugins/forecast-of-response/src/Types.ts
@@ -1,35 +1,21 @@
+import { DmtSettings } from '@dmt/core-plugins'
+
 export type TRoute = {
   path: string
   content: string
 }
 
-// TODO: Move to DMT and import from there
-export type TSettings = {
-  name: string
-  label: string
-  tabIndex: number
-  hidden: boolean
-  visibleDataSources: any
-  type: string
-  description: string
-  packages: any
-  models: any
-  actions: any
-  file_loc: string
-  data_source_aliases: any
-}
-
 export type TApp = {
   applications: any
-  settings: TSettings
+  settings: DmtSettings
 }
 
 export type TContent = {
   content: string
-  settings: TSettings
+  settings: DmtSettings
 }
 
 export type TLayout = {
   content: string
-  settings: TSettings
+  settings: DmtSettings
 }
