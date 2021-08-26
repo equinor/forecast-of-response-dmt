a7bb53921c8f805799c5797aed66fc4cf8054be3
netr0m <moamu@equinor.com>
2021-08-26 15:48:48 +0200

    placeholder for operation creation page

---
+++ web/custom-plugins/forecast-of-response/src/components/Operations/Create.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { DmtSettings } from '../../Types'
+
+export const OperationsCreate = (props: DmtSettings): JSX.Element => {
+  const { settings } = props
+  return (
+    <>
+      <b>Create a new op</b>
+    </>
+  )
+}
