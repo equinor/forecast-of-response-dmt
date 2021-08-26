9019f518ea8b8feac01ad3092ec183be05461cdc
netr0m <moamu@equinor.com>
2021-08-26 15:49:04 +0200

    placeholder for dashboard page

---
+++ web/custom-plugins/forecast-of-response/src/components/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { DmtSettings } from '../Types'
+
+export const Dashboard = (props: DmtSettings): JSX.Element => {
+  const { settings } = props
+  return (
+    <>
+      <i>My dashboard</i>
+    </>
+  )
+}
