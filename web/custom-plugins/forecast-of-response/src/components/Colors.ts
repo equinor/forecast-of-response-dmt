277b7ada2f379cda110b76a687dcee126f219934
netr0m <moamu@equinor.com>
2021-08-26 15:44:33 +0200

    moved to dedicated directory, simplified imports of colors from eds-tokens

--- web/custom-plugins/forecast-of-response/src/components/Colors.ts
+++
@@ -1,4 +0,0 @@
-import { tokens } from '@equinor/eds-tokens'
-
-export const backgroundColorDefault = tokens.colors.ui.background__default.rgba
-export const backgroundColorLight = tokens.colors.ui.background__light.rgba
---
+++ web/custom-plugins/forecast-of-response/src/components/Design/Colors.ts
@@ -0,0 +1,6 @@
+import { tokens } from '@equinor/eds-tokens'
+
+const { colors } = tokens
+
+export const backgroundColorDefault = colors.ui.background__default.rgba
+export const backgroundColorLight = colors.ui.background__light.rgba
