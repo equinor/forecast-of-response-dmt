ae33334f73cf610e31ec2a05b6e54864017af431
netr0m <moamu@equinor.com>
2021-08-27 15:26:13 +0200

    dummy "operations" data for testing layout

---
+++ web/custom-plugins/forecast-of-response/src/DummyData.tsx
@@ -0,0 +1,44 @@
+import { TOperation } from './Types'
+
+export const dummyOperations: Array<TOperation> = [
+  {
+    name: 'draft #1',
+    startDate: 1635242990,
+    endDate: undefined,
+    location: 'Gullfaks',
+    author: 'netr0m',
+    status: 'Draft',
+  },
+  {
+    name: 'njord pipe change',
+    startDate: 1631700590,
+    endDate: undefined,
+    location: 'Njord',
+    author: 'netr0m',
+    status: 'Draft',
+  },
+  {
+    name: 'mooring replacement',
+    startDate: 1556705390,
+    endDate: 1637924990,
+    location: 'Nordsjøen',
+    author: 'netr0m',
+    status: 'Ongoing',
+  },
+  {
+    name: 'replace broken rotor',
+    startDate: 1556705390,
+    endDate: 1637924990,
+    location: 'Nordsjøen',
+    author: 'netr0m',
+    status: 'Ongoing',
+  },
+  {
+    name: 'Troll wind farm',
+    startDate: 0,
+    endDate: 3153600000,
+    location: 'Troll',
+    author: 'netr0m',
+    status: 'Completed',
+  },
+]
