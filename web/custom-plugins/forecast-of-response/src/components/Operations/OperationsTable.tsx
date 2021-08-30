36f0365d40ced56d7004f5ac8b18c34daa8c1571
netr0m <moamu@equinor.com>
2021-08-30 13:14:21 +0200

    operationstable component

---
+++ web/custom-plugins/forecast-of-response/src/components/Operations/OperationsTable.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { DynamicTable } from '../DynamicTable'
+import { TOperation } from '../../Types'
+
+const columns: Array<string> = [
+  'Operation name',
+  'Date from',
+  'Date to',
+  'Location',
+  'Author',
+  'Status',
+]
+
+/**
+ * Takes a parameter date and converts into a string representation
+ * @param date Any value representing a date, such as a date string or ms since epoch
+ */
+const formatDate = (date: string | number | undefined): string => {
+  if (date === undefined) {
+    return '-'
+  }
+  try {
+    return new Date(date).toDateString()
+  } catch {
+    return `Unable to parse dateString '${date}'`
+  }
+}
+
+export const OperationsTable = (props: {
+  operations: Array<TOperation>
+}): JSX.Element => {
+  const { operations } = props
+  operations.forEach((operation: TOperation) => {
+    operation.start = formatDate(operation.start)
+    operation.end = formatDate(operation.end)
+    // TODO: Handle in a better way
+    // For some reason this loops twice, which causes it to map
+    // operation.location from a TLocation to the string name,
+    // then repeated but on the new string value
+    operation.location =
+      typeof operation.location === 'string'
+        ? operation.location
+        : operation.location.name
+  })
+
+  return (
+    <>
+      <DynamicTable columns={columns} rows={operations} />
+    </>
+  )
+}
