f4a0da430641fd0b7055647635aa04b312592bb2
netr0m <moamu@equinor.com>
2021-08-30 15:58:09 +0200

    hook for performing search

---
+++ web/custom-plugins/forecast-of-response/src/hooks/useSearch.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import { DocumentAPI } from '@dmt/common'
+
+const DEFAULT_DATASOURCE_ID = 'ForecastDS'
+
+export const useSearch = (
+  type: string,
+  dataSourceId: string = DEFAULT_DATASOURCE_ID
+): any => {
+  const [searchResult, setSearchResult] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
+  const documentAPI = new DocumentAPI()
+
+  useEffect(() => {
+    setIsLoading(true)
+    documentAPI
+      .search(dataSourceId, {
+        type: type,
+      })
+      .then((result: any) => {
+        setIsLoading(false)
+        setSearchResult(Object.values(result))
+      })
+      .catch((err: any) => {
+        console.error(err)
+        setIsLoading(false)
+        setHasError(true)
+      })
+  }, [dataSourceId, type])
+
+  return [searchResult, isLoading, setSearchResult, hasError]
+}
