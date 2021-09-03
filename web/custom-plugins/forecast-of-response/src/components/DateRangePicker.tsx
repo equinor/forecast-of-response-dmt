166c721cbb802aa39de60904d353ba1705212d96
netr0m <moamu@equinor.com>
2021-09-03 10:14:00 +0200

    resolve ts complaints

--- web/custom-plugins/forecast-of-response/src/components/DateRangePicker.tsx
+++ web/custom-plugins/forecast-of-response/src/components/DateRangePicker.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { DatePicker, Space } from 'antd'
+import { Label } from '@equinor/eds-core-react'
+import styled from 'styled-components'
 
 const { RangePicker } = DatePicker
+
+const Div = styled.div``
 
 export const DateRangePicker = (props: any): JSX.Element => {
   return (
     <>
-      <div>
-        <label className="sc-dIvrsQ kwOJXh">
-          <span>Specify date</span>
-        </label>
+      <Div>
+        <Label label="Specify date" />
         <Space direction="vertical" size={12}>
           <RangePicker />
         </Space>
-      </div>
+      </Div>
     </>
   )
 }
