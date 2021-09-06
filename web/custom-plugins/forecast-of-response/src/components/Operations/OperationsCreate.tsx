9e831abf5a6e07dc6dfd40347ab4ee6c80d6038a
netr0m <moamu@equinor.com>
2021-09-06 22:14:12 +0200

    added support for creation of new Location and OperationConfigs in DMSS, wrapped in Promise, minor cleanup

--- web/custom-plugins/forecast-of-response/src/components/Operations/OperationsCreate.tsx
+++ web/custom-plugins/forecast-of-response/src/components/Operations/OperationsCreate.tsx
@@ -3,16 +3,12 @@ import { Button, TextField } from '@equinor/eds-core-react'
 import styled from 'styled-components'
 import Grid from '../App/Grid'
 import { Heading } from '../Design/Fonts'
-import {
-  DmtSettings,
-  TLocation,
-  TOperation,
-  TOperationConfig,
-} from '../../Types'
+import { DmtSettings, TLocation, TOperationConfig } from '../../Types'
 import { OperationStatus } from '../../Enums'
 import { SelectOperationLocation } from './SelectLocation'
 import { SelectOperationConfig } from './SelectConfig'
-import { insertDocument } from '../../hooks/insertDocument'
+import { DateRangePicker } from '../DateRangePicker'
+import { insertDocument } from '../../utils/insertDocument'
 
 const Div = styled.div``
 
@@ -34,21 +30,63 @@ const SelectOperationName = (props: { setOperationName: any }): JSX.Element => {
   )
 }
 
-const prepareOperationEntity = (
+/**
+ * Create a new Location or retrieve the Id of an existing one
+ * @param locationEntity A Location entity
+ * @param isNew true if the Location needs to be created in DMSS
+ */
+const createOrGetLocationEntity = (
+  locationEntity: TLocation,
+  isNew: boolean = false
+): PromiseLike<string> => {
+  if (isNew) {
+    return insertDocument(locationEntity)
+  } else {
+    return new Promise((resolve: any) => {
+      resolve(locationEntity._id)
+    })
+  }
+}
+
+/**
+ * Create a new OperationConfig or retrieve the Id of an existing one
+ * @param configEntity An OperationConfig entity
+ * @param isNew true if the OperationConfig needs to be created in DMSS
+ */
+const createOrGetOperationConfigEntity = (
+  configEntity: TOperationConfig,
+  isNew: boolean = false
+): PromiseLike<string> => {
+  if (isNew) {
+    return insertDocument(configEntity)
+  } else {
+    return new Promise((resolve: any) => {
+      resolve(configEntity._id)
+    })
+  }
+}
+
+/**
+ * Create a new Operation entity
+ * @param operationName Name of the operation to create
+ * @param dateRange An array of dates as [startDate, endDate]
+ * @param operationConfig An OperationConfig entity
+ * @param location A Location entity
+ */
+const createOperationEntity = (
   operationName: string,
+  dateRange: Date[],
   operationConfig: TOperationConfig,
-  location: TLocation,
-  setOperation: any
-) => {
-  setOperation({
-    _id: 'testInsert', // TODO: Produce a unique ID
+  location: TLocation
+): PromiseLike<string> => {
+  return insertDocument({
     name: operationName,
     type: 'ForecastDS/ForecastOfResponse/Blueprints/Operation',
     description: '', // TODO: Add description input?
     creator: 'someUser', // TODO: Get user from current session, or automatically in the backend based on token?
     location: location,
-    start: new Date().toISOString(), // TODO: Add start/end date selectors
-    end: undefined,
+    start: dateRange && dateRange[0] ? dateRange[0].toISOString() : undefined,
+    end: dateRange && dateRange[1] ? dateRange[1].toISOString() : undefined,
     status: OperationStatus.UPCOMING, // TODO: decide based on start attr? allow user to select?
     config: operationConfig,
   })
@@ -56,16 +94,14 @@ const prepareOperationEntity = (
 
 export const OperationsCreate = (props: DmtSettings): JSX.Element => {
   const { settings } = props
-  const [operation, setOperation] = useState<TOperation>()
-  const [operationName, setOperationName] = useState<string>()
+  const [error, setError] = useState<string>()
+  const [operationMeta, setOperationMeta] = useState<{
+    name: string
+    dateRange: Date[]
+  }>()
   const [operationConfig, setOperationConfig] = useState<TOperationConfig>()
-  const [selectedLocation, setSelectedLocation] = useState<TLocation>({})
+  const [operationLocation, setOperationLocation] = useState<TLocation>({})
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [hasClickedCreate, setHasClickedCreate] = useState<boolean>(false)
-  const [response, isUploading, hasError] = insertDocument(
-    operation,
-    hasClickedCreate
-  )
 
   // TODO: redirect to operation view upon creation
   // TODO: Create new location if not exists (currently overwrites topmost location)
@@ -77,36 +113,109 @@ export const OperationsCreate = (props: DmtSettings): JSX.Element => {
     <>
       <Grid>
         <Div>
-          <SelectOperationName setOperationName={setOperationName} />
+          <SelectOperationName
+            setOperationName={(operationName: string) => {
+              // @ts-ignore-line
+              setOperationMeta(
+                Object.assign({}, operationMeta, { name: operationName })
+              )
+            }}
+          />
+          <br />
+          <Heading text="Select start and end date" variant="h4" />
+          <DateRangePicker
+            setDateRange={(dateRange: Date[]) => {
+              // @ts-ignore-line
+              setOperationMeta(
+                Object.assign({}, operationMeta, { dateRange: dateRange })
+              )
+            }}
+          />
           <br />
           <SelectOperationConfig
-            operationConfig={operationConfig}
             setOperationConfig={setOperationConfig}
             isLoading={isLoading}
             setIsLoading={setIsLoading}
           />
         </Div>
         <SelectOperationLocation
-          selectedLocation={selectedLocation}
-          setSelectedLocation={setSelectedLocation}
+          location={operationLocation}
+          setLocation={setOperationLocation}
         />
       </Grid>
       <Button
         onClick={() => {
-          if (operationConfig && !isLoading) {
-            prepareOperationEntity(
-              operationName,
-              operationConfig,
-              selectedLocation,
-              setOperation
+          if (
+            operationMeta?.name &&
+            operationLocation?.name &&
+            operationConfig?.name &&
+            !isLoading
+          ) {
+            // Prepare the uncontained entities for the Operation
+            const locationEntity: TLocation = {
+              _id: operationLocation._id ? operationLocation._id : undefined,
+              name: operationLocation.name,
+              type: 'ForecastDS/ForecastOfResponse/Blueprints/Location',
+              UTM: operationLocation.UTM,
+            }
+            const configEntity: TOperationConfig = {
+              _id: operationConfig._id ? operationConfig._id : undefined,
+              name: operationConfig.name,
+              type: 'ForecastDS/ForecastOfResponse/Blueprints/OperationConfig',
+            }
+            Promise.all([
+              createOrGetLocationEntity(
+                locationEntity,
+                !!operationLocation.isNew
+              ),
+              createOrGetOperationConfigEntity(
+                configEntity,
+                !!operationConfig.isNew
+              ),
+            ])
+              .then((documentIds: string[]) => {
+                delete locationEntity.UTM
+                locationEntity._id = documentIds[0]
+                configEntity._id = documentIds[1]
+                createOperationEntity(
+                  operationMeta.name,
+                  operationMeta.dateRange,
+                  configEntity,
+                  locationEntity
+                )
+                  .then((documentId) => {
+                    console.log(`New operation ${documentId}`)
+                  })
+                  .catch((err: any) => {
+                    console.log(err)
+                  })
+              })
+              .catch((err: any) => {
+                console.error(err)
+              })
+          } else {
+            const missing = []
+            if (!operationMeta?.name) {
+              missing.push('Operation name')
+            }
+            if (!operationLocation?._id) {
+              missing.push('Location')
+            }
+            if (!operationConfig?._id) {
+              missing.push('Configuration')
+            }
+            setError(
+              `Missing required ${
+                missing.length > 1 ? 'fields' : 'field'
+              } ${missing.join(', ')}`
             )
-            setHasClickedCreate(true)
           }
         }}
       >
         Create operation
       </Button>
       <Button color="secondary">Cancel</Button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </>
   )
 }
