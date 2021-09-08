32e0c093d16ff6f50d54287e4d0b975417a7aa77
netr0m <moamu@equinor.com>
2021-09-08 10:56:14 +0200

    return as object to allow selecting attributes, cleanup

--- web/custom-plugins/forecast-of-response/src/hooks/useSearch.tsx
+++ web/custom-plugins/forecast-of-response/src/hooks/useSearch.tsx
@@ -8,12 +8,10 @@ export const useSearch = (
   dataSourceId: string = DEFAULT_DATASOURCE_ID
 ): any => {
   const [searchResult, setSearchResult] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
   const [hasError, setHasError] = useState(false)
   const documentAPI = new DocumentAPI()
 
   useEffect(() => {
-    setIsLoading(true)
     documentAPI
       .search(dataSourceId, {
         type: type,
@@ -21,14 +19,12 @@ export const useSearch = (
       .then((result: any) => {
         // @ts-ignore-line
         setSearchResult(Object.values(result))
-        setIsLoading(false)
       })
       .catch((err: any) => {
         console.error(err)
         setHasError(true)
-        setIsLoading(false)
       })
   }, [dataSourceId, type])
 
-  return [searchResult, isLoading, setSearchResult, hasError]
+  return { result: searchResult, hasError: hasError }
 }
