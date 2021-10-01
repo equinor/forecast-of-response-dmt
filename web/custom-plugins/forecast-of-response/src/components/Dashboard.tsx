5d6dd5e4f55e5ec9f03375ac714cd48808bf92f2
Stig Ofstad <stigofstad@gmail.com>
2021-10-01 14:07:09 +0200

    ACL component

--- web/custom-plugins/forecast-of-response/src/components/Dashboard.tsx
+++ web/custom-plugins/forecast-of-response/src/components/Dashboard.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { DmtSettings } from '../Types'
+import AccessControlList from "./AccessControl/AccessControlList"
 
 export const Dashboard = (props: DmtSettings): JSX.Element => {
   const { settings } = props
   return (
     <>
       <i>My dashboard</i>
+      <AccessControlList/>
     </>
   )
 }
