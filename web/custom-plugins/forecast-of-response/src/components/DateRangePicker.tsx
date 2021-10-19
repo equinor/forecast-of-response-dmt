6ade8a760252cf217498e6c3fff62a4d261ad23c
Stig Ofstad <stigofstad@gmail.com>
2021-10-19 10:34:44 +0200

    create operation - styling, loc from map, bugs

--- web/custom-plugins/forecast-of-response/src/components/DateRangePicker.tsx
+++ web/custom-plugins/forecast-of-response/src/components/DateRangePicker.tsx
@@ -8,18 +8,16 @@ const DateRangePicker = (props: { setDateRange: Function }): JSX.Element => {
   const { setDateRange } = props
   // onChange returns (date: Moment[], dateString: string[]) with start at [0] and end at [1]
   return (
-    <>
-      <div>
-        <Label label="Specify date" />
-        <Space direction="vertical" size={12}>
-          <RangePicker
-            onChange={(dates: any[]) => {
-              setDateRange([dates[0].toDate(), dates[1].toDate()])
-            }}
-          />
-        </Space>
-      </div>
-    </>
+    <div>
+      <Label label="Specify date" />
+      <Space direction="vertical" size={12}>
+        <RangePicker
+          onChange={(dates: any[]) => {
+            setDateRange([dates[0].toDate(), dates[1].toDate()])
+          }}
+        />
+      </Space>
+    </div>
   )
 }
 
--- web/custom-plugins/forecast-of-response/src/components/Operations/SelectConfig.tsx
+++ web/custom-plugins/forecast-of-response/src/components/Operations/SelectConfig.tsx
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Progress, SingleSelect } from '@equinor/eds-core-react'
 import { TConfig } from '../../Types'
 import useSearch from '../../hooks/useSearch'
-import { Heading, Meta } from '../Design/Fonts'
+import { Heading } from '../Design/Fonts'
 import Grid from '../App/Grid'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: min-content;
+  max-width: 400px;
+`
+
+const LocationButtonsGrid = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  max-width: 300px;
+`
 
 /**
  * Read the contents of a file as text
@@ -29,10 +43,8 @@ const SelectOperationConfig = (props: {
   const [searchResult, isLoadingSearch, hasError] = useSearch(
     'ForecastDS/ForecastOfResponse/Blueprints/Config'
   )
-  const [
-    operationConfigUploadFileName,
-    setOperationConfigUploadFileName,
-  ] = useState<string>()
+  const [fileName, setFileName] = useState<string>()
+  const [uploadNew, setUploadNew] = useState<boolean>(true)
   const { setOperationConfig, setIsNewConfig, isLoading, setIsLoading } = props
 
   /**
@@ -45,65 +57,89 @@ const SelectOperationConfig = (props: {
   }, [searchResult])
 
   return (
-    <>
+    <Wrapper>
       <Heading text="Configuration file" variant="h4" />
       <Grid>
-        <SingleSelect
-          id="operationConfigSelector"
-          label="Select operation config from library"
-          items={operationConfigs?.map((opConfig: TConfig) => opConfig.name)}
-          handleSelectedItemChange={(event: any) => {
-            setIsNewConfig(false)
-            setOperationConfig(
-              operationConfigs.find(
-                (config: TConfig) => config.name === event.selectedItem
-              )
-            )
-          }}
-        />
-        <div>
-          <input
-            type="file"
-            id="operationConfigUpload"
-            style={{ display: 'none' }}
-            accept=".json"
-            onChange={(event: any) => {
-              if (event.target.files.length >= 1) {
-                const file = event.target.files[0]
-                if (file.type === 'application/json') {
-                  setOperationConfigUploadFileName(file.name)
-                  setIsLoading(true)
-                  readFile(file)
-                    .then((contents: string) => {
-                      const configJson = JSON.parse(contents)
-                      setIsNewConfig(true)
-                      setOperationConfig(configJson)
-                      setIsLoading(false)
-                    })
-                    .catch((err: any) => {
-                      console.error(err)
-                      setIsLoading(false)
-                    })
-                } else {
-                  console.error(
-                    'Specified file is not in the required format (JSON).'
-                  )
+        <LocationButtonsGrid>
+          <Button
+            variant={!uploadNew ? 'contained' : 'outlined'}
+            onClick={() => setUploadNew(false)}
+          >
+            Select existing
+          </Button>
+          <Button
+            variant={uploadNew ? 'contained' : 'outlined'}
+            onClick={() => setUploadNew(true)}
+          >
+            New
+          </Button>
+        </LocationButtonsGrid>
+        {uploadNew ? (
+          <div>
+            <input
+              type="file"
+              id="operationConfigUpload"
+              style={{ display: 'none' }}
+              accept=".json"
+              onChange={(event: any) => {
+                if (event.target.files.length >= 1) {
+                  const file = event.target.files[0]
+                  if (file.type === 'application/json') {
+                    setFileName(file.name)
+                    setIsLoading(true)
+                    readFile(file)
+                      .then((contents: string) => {
+                        const configJson = JSON.parse(contents)
+                        setIsNewConfig(true)
+                        setOperationConfig(configJson)
+                        setIsLoading(false)
+                      })
+                      .catch((err: any) => {
+                        console.error(err)
+                        setIsLoading(false)
+                      })
+                  } else {
+                    console.error(
+                      'Specified file is not in the required format (JSON).'
+                    )
+                  }
                 }
-              }
+              }}
+            />
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <label htmlFor="operationConfigUpload">
+                <Button as="span" variant="outlined">
+                  {(isLoading && <Progress.Dots color="primary" />) ||
+                    'Upload configuration'}
+                </Button>
+              </label>
+              <div
+                style={{
+                  marginLeft: '5px',
+                  alignSelf: 'flex-end',
+                }}
+              >
+                Selected: <i>{fileName || 'None...'}</i>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <SingleSelect
+            id="operationConfigSelector"
+            label="Select operation config from library"
+            items={operationConfigs?.map((opConfig: TConfig) => opConfig.name)}
+            handleSelectedItemChange={(event: any) => {
+              setIsNewConfig(false)
+              setOperationConfig(
+                operationConfigs.find(
+                  (config: TConfig) => config.name === event.selectedItem
+                )
+              )
             }}
           />
-          <label htmlFor="operationConfigUpload">
-            <Button as="span" variant="outlined">
-              {(isLoading && <Progress.Dots color="primary" />) ||
-                'Upload new config'}
-            </Button>
-          </label>
-          {operationConfigUploadFileName && (
-            <Meta text={`File: ${operationConfigUploadFileName}`} />
-          )}
-        </div>
+        )}
       </Grid>
-    </>
+    </Wrapper>
   )
 }
 
