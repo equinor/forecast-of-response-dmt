a5f1569367f2d0ae1d20f5e52a6a8409978f417e
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2021-10-27 12:27:36 +0200

    start on menu behavior

--- web/custom-plugins/forecast-of-response/src/components/App/Menu.tsx
+++ web/custom-plugins/forecast-of-response/src/components/App/Menu.tsx
@@ -11,6 +11,7 @@ const { SubMenu } = Menu
 export default (props: { appRootPath: string }): JSX.Element => {
   const { appRootPath } = props
   const [collapsed, setCollapsed] = useState(false)
+  const iconSize: 24 | 16 | 32 | 40 | 48 | undefined = 24
 
   return (
     <Sider
@@ -21,7 +22,7 @@ export default (props: { appRootPath: string }): JSX.Element => {
       onCollapse={(collapsed: boolean) => setCollapsed(collapsed)}
     >
       <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
-        <Menu.Item key="1" icon={<Icon name="home" size={24} />}>
+        <Menu.Item key="1" icon={<Icon name="home" size={iconSize} />}>
           <Link to={`/${appRootPath}`}>Dashboard</Link>
         </Menu.Item>
         {/*<SubMenu*/}
@@ -29,18 +30,18 @@ export default (props: { appRootPath: string }): JSX.Element => {
         {/*  icon={<Icon name="library" size={24} />}*/}
         {/*  title="Library"*/}
         {/*>*/}
-        <Menu.Item key="3">
+        <Menu.Item key="3" icon={<Icon name="library" size={iconSize} />}>
           <Link to={`/${appRootPath}/library`}>Stask Library</Link>
         </Menu.Item>
         {/*</SubMenu>*/}
-        <Menu.Item key="4" icon={<Icon name="list" size={24} />}>
+        <Menu.Item key="4" icon={<Icon name="list" size={iconSize} />}>
           <Link to={`/${appRootPath}/operations`}>Operation overview</Link>
         </Menu.Item>
-        <Menu.Item key="8">
-          <Link to={`/${appRootPath}/operations/new`}>
-            <Button>Create new operation</Button>
-          </Link>
-        </Menu.Item>
+        {/*<Menu.Item key="8">*/}
+        {/*  <Link to={`/${appRootPath}/operations/new`}>*/}
+        {/*    <Button>Create new operation</Button>*/}
+        {/*  </Link>*/}
+        {/*</Menu.Item>*/}
         {/*</SubMenu>*/}
       </Menu>
     </Sider>
