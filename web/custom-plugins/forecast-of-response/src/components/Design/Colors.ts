61b1dde259a4548af33cbe8795dff4e1f6b4ebc8
Stig Ofstad <stigofstad@gmail.com>
2021-10-28 14:53:41 +0200

    added summary of a simulation config

--- web/custom-plugins/forecast-of-response/src/components/Design/Colors.ts
+++ web/custom-plugins/forecast-of-response/src/components/Design/Colors.ts
@@ -6,3 +6,4 @@ export const backgroundColorDefault = colors.ui.background__default.rgba
 export const backgroundColorLight = colors.ui.background__light.rgba
 export const primary = colors.infographic.primary__moss_green_100.rgba
 export const primaryGray = '#f7f7f7'
+export const lightGray = '#f6f6f6'
