74a6d599bbf2f0d06b3726b47c23b16f8f817f0d
Stig Ofstad <stigofstad@gmail.com>
2021-10-29 15:54:44 +0200

    trigger sre jobs from Forecast of Response. View logs

---
+++ web/custom-plugins/forecast-of-response/src/components/Jobs.tsx
@@ -0,0 +1,71 @@
+import React, { useContext, useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { AuthContext } from '@dmt/common'
+import JobApi from '../utils/JobApi'
+import { SimulationStatus } from '../Enums'
+
+const StyledPre = styled.pre`
+  display: flex;
+  white-space: pre-line;
+`
+
+function colorFromStatus(status: string): string {
+  switch (status) {
+    case SimulationStatus.COMPLETED:
+      return 'green'
+    case SimulationStatus.FAILED:
+      return 'red'
+    case SimulationStatus.RUNNING:
+      return 'orange'
+    default:
+      return 'darkgrey'
+  }
+}
+
+const SimStatusWrapper = styled.div`
+  display: flex;
+  border-radius: 8px;
+  padding: 0 3px;
+  margin-left: 10px;
+  border: ${(props: any) => `${colorFromStatus(props.status)} 3px solid`};
+  color: ${(props: any) => colorFromStatus(props.status)};
+  font-weight: bolder;
+`
+
+export const JobLog = (props: { jobId: string }) => {
+  const { jobId } = props
+  const { token } = useContext(AuthContext)
+  const jobAPI = new JobApi(token)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [jobLogs, setJobLogs] = useState<any>()
+  const [jobStatus, setJobStatus] = useState<any>()
+
+  useEffect(() => {
+    setLoading(true)
+    jobAPI
+      .statusJob(jobId)
+      .then((result: any) => {
+        setJobLogs(result.data.log)
+        setJobStatus(result.data.status)
+      })
+      .catch((e: Error) => {
+        setJobLogs(e.response.data.message)
+        setJobStatus(SimulationStatus.FAILED)
+        console.error(e)
+      })
+      .finally(() => setLoading(false))
+  }, [jobId])
+
+  if (loading) return <pre>Loading...</pre>
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <label>Status: </label>
+        <SimStatusWrapper status={jobStatus}>{jobStatus}</SimStatusWrapper>
+      </div>
+      <label>Logs:</label>
+      <StyledPre>{jobLogs}</StyledPre>
+    </div>
+  )
+}
