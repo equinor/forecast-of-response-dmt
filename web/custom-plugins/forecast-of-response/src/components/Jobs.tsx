7bbb617e3b8ec048d7355e76725bf3eee35ee118
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2021-12-20 14:13:35 +0100

    style UI for phase view

--- web/custom-plugins/forecast-of-response/src/components/Jobs.tsx
+++ web/custom-plugins/forecast-of-response/src/components/Jobs.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { AuthContext } from '@dmt/common'
 import JobApi from '../utils/JobApi'
 import { SimulationStatus } from '../Enums'
-import { Label } from '@equinor/eds-core-react'
+import { Button, Label } from '@equinor/eds-core-react'
 
 const StyledPre = styled.pre`
   display: flex;
@@ -28,7 +28,7 @@ const SimStatusWrapper = styled.div`
   height: 20px;
   align-content: baseline;
   border-radius: 5px;
-  padding: 0 3px;
+  padding: 0 3px 3px 0;
   margin-left: 10px;
   border: ${(props: any) => `${colorFromStatus(props.status)} 2px solid`};
   color: ${(props: any) => colorFromStatus(props.status)};
@@ -41,6 +41,7 @@ export const JobLog = (props: { jobId: string }) => {
   const [loading, setLoading] = useState<boolean>(false)
   const [jobLogs, setJobLogs] = useState<any>()
   const [jobStatus, setJobStatus] = useState<any>()
+  const [refreshCount, setRefreshCount] = useState<number>(0)
 
   useEffect(() => {
     setLoading(true)
@@ -56,18 +57,31 @@ export const JobLog = (props: { jobId: string }) => {
         console.error(e)
       })
       .finally(() => setLoading(false))
-  }, [jobId])
+  }, [jobId, refreshCount])
 
   if (loading) return <pre>Loading...</pre>
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div style={{ display: 'flex', flexDirection: 'row', marginTop: '10px' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
         <Label label="Status:" />
         <SimStatusWrapper status={jobStatus}>{jobStatus}</SimStatusWrapper>
+        <Button
+          onClick={() => setRefreshCount(refreshCount + 1)}
+          style={{ marginLeft: '100px' }}
+        >
+          Refresh logs
+        </Button>
       </div>
-      <Label label="Logs:" />
-      <StyledPre>{jobLogs}</StyledPre>
+      <Label style={{ paddingTop: '10px' }} label="Logs:" />
+      <div style={{ paddingBottom: '20px' }}>
+        <StyledPre>{jobLogs}</StyledPre>
+      </div>
     </div>
   )
 }
