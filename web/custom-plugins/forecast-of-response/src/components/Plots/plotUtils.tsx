b38166464b223ccf94d87755a07b943da64c0618
Kristian Kjerstad <69512295+KristianKjerstad@users.noreply.github.com>
2022-02-15 16:28:49 +0100

    improve plot (shading, display current time) (#228)
    * add shading to differentiate different days
    * display current time as a vertical line
    * update time format

--- web/custom-plugins/forecast-of-response/src/components/Plots/plotUtils.tsx
+++ web/custom-plugins/forecast-of-response/src/components/Plots/plotUtils.tsx
@@ -4,10 +4,19 @@ export const getTime = (
   useLocalTimezone: boolean
 ): string => {
   if (!issueWithTimeFormat) {
+    let formatOptions: {} = {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }
     if (useLocalTimezone) {
-      return new Date(dateString).toLocaleString(navigator.language)
+      return new Date(dateString).toLocaleString('en-GB', formatOptions)
     } else {
-      return `${new Date(dateString).toLocaleString(navigator.language, {
+      return `${new Date(dateString).toLocaleString('en-GB', {
+        ...formatOptions,
         timeZone: 'UTC',
       })}`
     }
@@ -19,4 +28,18 @@ export const getDayFromDateString = (dateString: string) => {
   return new Date(dateString).toLocaleString('en-us', {
     weekday: 'long',
   })
+}
+
+export function dayOfYear(date: string) {
+  // Get the day of year, a number between 1 and 365 (364 for leap year)
+  // source: https://stackoverflow.com/questions/8619879/javascript-calculate-the-day-of-the-year-1-366/8619946#8619946
+  var now: Date = new Date(date)
+  var start: Date = new Date(now.getFullYear(), 0, 0)
+  //@ts-ignore
+  var diff =
+    now -
+    start +
+    (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000
+  var oneDay = 1000 * 60 * 60 * 24
+  return Math.floor(diff / oneDay)
 }
